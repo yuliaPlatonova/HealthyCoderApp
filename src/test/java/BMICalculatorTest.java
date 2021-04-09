7d507d229af329acaa74e45753598fe8aa256b4e
Yulia Platonova <julia.ikkunaprincessa@gmail.com>
2021-04-09 12:18:32 -0700

    Section 3  @CsvFileSource example

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,7 +26,7 @@ class BMICalculatorTest {
     }
 
     @ParameterizedTest(name = "weight{0}, height{1}")
-    @CsvSource(value = {"89.0, 1.72", "95.0, 1.65", "110.0, 1.78"})
+    @CsvFileSource(resources = "/diet-recommended-input-data.csv", numLinesToSkip = 1)
     void should_ReturnTrue_When_DietRecommended(Double coderWeight, Double coderHeight) {
         //given
         double weight = coderWeight;
