ccae2a7582947f1fc2ee6fa719eb1db1828c4722
Yulia Platonova <julia.ikkunaprincessa@gmail.com>
2021-04-09 11:21:25 -0700

    Sections 3 example with Parametrized test

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +23,11 @@ class BMICalculatorTest {
         System.out.println("After all unit tests");
     }
 
-    @Test
-    void should_ReturnTrue_When_DietRecommended() {
+    @ParameterizedTest
+    @ValueSource(doubles = {89.0, 95.0, 110.0})
+    void should_ReturnTrue_When_DietRecommended(Double coderWeight) {
         //given
-        double weight = 89.0;
+        double weight = coderWeight;
         double height = 1.72;
 
         //when
